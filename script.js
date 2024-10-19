96f5ec47223de01a74d1cf8b1fff07ef29d28407
Alysson Rodrigues <154292274+thisisalyssonx@users.noreply.github.com>
2024-10-19 12:14:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-// Função para atualizar o relógio
 function updateClock() {
     const hoursElement = document.getElementById('hours');
     const minutesElement = document.getElementById('minutes');
     const secondsElement = document.getElementById('seconds');
     const greetingElement = document.getElementById('greeting');
     const bodyElement = document.body;
-
-    // Obtém o horário atual
+    
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
 
-    // Atualiza os elementos do relógio com o tempo formatado
     hoursElement.textContent = padTime(hours);
     minutesElement.textContent = padTime(minutes);
     secondsElement.textContent = padTime(seconds);
 
-    // Define a saudação e o fundo com base no horário
     if (hours >= 6 && hours < 12) {
         greetingElement.textContent = 'Bom dia!';
         bodyElement.style.backgroundImage = "url('imgs/manha.png')";
@@ -30,13 +26,10 @@ function updateClock() {
     }
 }
 
-// Função para adicionar um zero à esquerda, se necessário
 function padTime(time) {
     return time < 10 ? '0' + time : time;
 }
 
-// Atualiza o relógio a cada segundo
 setInterval(updateClock, 1000);
 
-// Chama a função uma vez ao carregar a página
 updateClock();
